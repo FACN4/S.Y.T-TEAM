7d6b5d2de8a975c88a942f86b87cc1f10c36d972
solk10 <sol.1919@yahoo.com>
2018-08-13 23:15:23 +0300

    Shell >#1

---
+++ src/server.js
@@ -0,0 +1,11 @@
+const https = require('https');
+// const router = require('./router');
+
+/* Creating server */
+const server = http.createServer(router);
+/* Port for local host is 9000, process.env allows for Heroku */
+const port = process.env.PORT || 4444;
+/* Set-up listen on the server, inputting the port and a callback to console log the port number. */
+server.listen(port, () => {
+  console.log(`Listening to port: ${port}`);
+});
