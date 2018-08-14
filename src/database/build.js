8549c9cc2ddde7a468814c6b528b1c9757174c53
solk10 <sol.1919@yahoo.com>
2018-08-14 16:35:31 +0300

    >#3 database almost set

---
+++ src/database/build.js
@@ -0,0 +1,33 @@
+
+const fs = require('fs');
+const dbConnection = require('./dbconnection');
+
+
+// const sgcT = fs.readFileSync(`${__dirname}/build.sql`, 'utf-8');
+//
+// const runDbBuild = (cb) => {
+//   dbConnection.query(makeEmptyTables, (error) => {
+//     if (error) {
+//       console.log('Building DB error', error);
+//       cb(error);
+//     } else {
+//       userData.forEach((person) => {
+//         if (person.avatar_url) {
+//           const SQLquery = 'INSERT INTO users (git_username,git_profile_url,git_photo_url) VALUES ($1,$2,$3)';
+//           dbConnection.query(SQLquery, [person.login, person.html_url, person.avatar_url],
+//             (err) => {
+//               if (err) {
+//                 cb(err);
+//               }
+//             });
+//         }
+//       });
+//       cb(null);
+//     }
+//   });
+// };
+//
+// if (process.argv[2] === 'run') {
+//   runDbBuild(console.log);
+// }
+// module.exports = runDbBuild;
