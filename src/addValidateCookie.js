4f33c12dd1aef888efed883728afde5940619365
tawfiknasser <tawfiknasser@gmail.com>
2018-08-16 00:32:04 +0300

    #6 added session

---
+++ src/addValidateCookie.js
@@ -0,0 +1,32 @@
+const env = require('env2')('config.env');
+const secret = process.env.SECRET;
+const jwt = require("jsonwebtoken");
+const cookie=require("cookie");
+module.exports = {
+  sign: (username, res) => {
+    const signedCookie = jwt.sign(username, secret);
+    res.writeHead(302, {
+      "set-cookie": `jwt=${signedCookie}`,
+      Location: "/post.html"
+    });
+    res.end();
+  },
+
+  validate: (cookieClient) => {
+
+    return new Promise(function(resolve, reject) {
+       var cookies = (cookie.parse(cookieClient)).jwt;
+      if (cookies) {
+        jwt.verify(cookies, secret, (err, result) => {
+          if (err) {
+            reject(new Error(err));
+          } else {
+            console.log("valdation about to resolve",result);
+            resolve(result);
+          }
+
+        });
+      }
+    });
+  }
+};
